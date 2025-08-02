b118b54503326a40b77eb7d4165c109e562868b9
MRLIGHT02 <saifali42020@gmail.com>
2025-08-02 11:30:32 +0530

    Add project files.

---
+++ Task.Run/Program.cs
@@ -0,0 +1,35 @@
+﻿class UpCounter {
+
+    public void CountUp(int count)
+    {
+        Console.WriteLine("Counting up Started:");
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine($"Count Up: {i + 1}");
+        }
+        Console.WriteLine("Counting up Finished");
+    }
+
+}
+
+class DownCounter {
+
+    public void CountDown(int count)
+    {
+        Console.WriteLine("Counting Down Started:");
+        for (int j = count; j <= 1; j--)
+        {
+            Console.WriteLine($"Count Down: {j + 1}");
+        }
+        Console.WriteLine("Counting Down Finished");
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+    // c
+    }
+}
